1e435411ccbd6b98a17e29cfc86ba78a12f149dd
Mateusz Miodek <mmiodek@gmail.com>
2018-02-06 20:34:55 +0100

    added active navigation class

--- app/js/navigation.js
+++ app/js/navigation.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-var pathname = window.location.pathname;
-var currentPage = pathname.split("/").pop();
-$('.nav>li>a').filter("[href=currentPage]").addClass("active");
+  var pathname = window.location.pathname;
+  var currentPage = pathname.split("/").pop();
+  $('.nav a[href="'+currentPage+'"]').parent().addClass("active");
 });
